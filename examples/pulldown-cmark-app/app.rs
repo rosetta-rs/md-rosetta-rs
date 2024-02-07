c5248a0cc0aa9a631ba12d7e2477652cfd251853
Ed Page <eopage@gmail.com>
2024-02-07 13:36:21 -0600

    fix: Don't print when benchmarking

--- examples/pulldown-cmark-app/app.rs
+++ examples/pulldown-cmark-app/app.rs
@@ -3,21 +3,31 @@ use std::io::Write;
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
     let mut args = std::env::args_os();
     let _bin = args.next();
-    if let Some(path) = args.next() {
-        let raw = std::fs::read_to_string(&path)?;
-        render(&raw)?;
+    let path = args.next().expect("no markdown path given");
+    let raw = std::fs::read_to_string(&path)?;
+    #[cfg(debug_assertions)]
+    {
+        let stdout = std::io::stdout();
+        let mut stdout = stdout.lock();
+        render(&mut stdout, &raw)?;
     }
-
+    #[cfg(not(debug_assertions))]
+    {
+        let mut buffer = Vec::new();
+        render(&mut buffer, &raw)?;
+        std::hint::black_box(buffer);
+    }
     Ok(())
 }
 
-fn render(raw: &str) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+fn render(
+    writer: &mut dyn Write,
+    raw: &str,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
     let parser = pulldown_cmark::Parser::new(raw);
 
-    let stdout = std::io::stdout();
-    let mut stdout = stdout.lock();
     for event in parser {
-        let _ = writeln!(stdout, "{:?}", event);
+        let _ = writeln!(writer, "{:?}", event);
     }
 
     Ok(())
