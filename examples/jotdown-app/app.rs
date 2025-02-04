358e17ef83495552153c0ae8d4b9840d4140ca80
Dawid Ciężarkiewicz <dpc@dpc.pw>
2025-02-03 18:52:07 -0800

    feat: add `jotdown` for comparison
    Djot is a "kind-of-more-strict-Markdown" that is (supposedly) make
    it faster and easier to parse. I am curious myself how well it
    performs.

---
+++ examples/jotdown-app/app.rs
@@ -0,0 +1,32 @@
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let mut args = std::env::args_os();
+    let _bin = args.next();
+    let path = args.next().expect("no markdown path given");
+    let raw = std::fs::read_to_string(path)?;
+    #[cfg(debug_assertions)]
+    {
+        let stdout = std::io::stdout();
+        let mut stdout = stdout.lock();
+        render(&mut stdout, &raw)?;
+    }
+    #[cfg(not(debug_assertions))]
+    {
+        let mut buffer = Vec::new();
+        render(&mut buffer, &raw)?;
+        std::hint::black_box(buffer);
+    }
+    Ok(())
+}
+
+fn render(
+    writer: &mut dyn Write,
+    raw: &str,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let events = jotdown::Parser::new(raw);
+    let html = jotdown::html::render_to_string(events);
+    let _ = writeln!(writer, "{:?}", html);
+
+    Ok(())
+}
